4722f1ef36971a9d56b034f1804d03511b371d06
prem-jeet <prem4300795@gmail.com>
2022-03-21 14:07:54 +0530

    refactor: contact form layout add: checkbox handler

---
+++ index.js
@@ -0,0 +1,9 @@
+function checked_status(e) {
+  let checked = e.checked;
+  const mobile_container = document.querySelector("#mobile_container");
+
+  // make the input for mobile number visible
+  // make hide the input for  mobile number
+  mobile_container.classList.toggle("d-none", !checked);
+  mobile_container.querySelector("#mobile").toggleAttribute("required", checked)
+}
