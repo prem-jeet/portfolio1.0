4f3af9f21e95346d1bd93846af678194dcc6028e
prem-jeet <prem4300795@gmail.com>
2022-03-27 10:51:43 +0530

    completed: all project cards

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const featured_project_carousel = document.querySelector(
 );
 let carousel = new bootstrap.Carousel(featured_project_carousel);
 
-
-
 play_pause_button.addEventListener("click", (e) => {
   let state = e.target.getAttribute("data-state");
 
@@ -13,14 +11,12 @@ play_pause_button.addEventListener("click", (e) => {
     e.target.classList.remove("fa-play");
     e.target.classList.add("fa-pause");
     e.target.setAttribute("data-state", "pause");
-    carousel.pause()
-    
-    
+    carousel.pause();
   } else {
     e.target.classList.remove("fa-pause");
     e.target.classList.add("fa-play");
     e.target.setAttribute("data-state", "play");
-    carousel.cycle()
+    carousel.cycle();
   }
 });
 
@@ -36,20 +32,52 @@ function checked_status(e) {
     .toggleAttribute("required", checked);
 }
 
-
-
 // * swiper js config
 var swiper = new Swiper(".swiper-container", {
-	effect: "coverflow",
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	coverflowEffect: {
-		rotate: 50,
-		stretch: 0,
-		depth: 100,
-		modifier: 1,
-		slideShadows: true
-	},
-
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
 });
+
+// * modularize the all project cards using template and jsons file
+
+const project_card_template = document.querySelector("#project_card_template");
+const swiper_wrapper = document.querySelector(".swiper-wrapper");
+const projects = require("./projects.json");
+
+for (project in projects) {
+  let new_card = makeCard(projects[project]);
+
+  swiper_wrapper.appendChild(new_card);
+}
+
+function makeCard(data) {
+  let new_card = project_card_template.content.cloneNode(true).children[0];
+
+  let project_img = new_card.querySelector(".project__img");
+  let project_title = new_card.querySelector(".project__title");
+  let github_button = new_card.querySelector(".project__button[github]");
+  let live_button = new_card.querySelector(".project__button[live]");
+  let project_description = new_card.querySelector(".project__description");
+  let project_tags = new_card.querySelector(".project__tags");
+
+  project_img.style.backgroundImage = `url(${data.img})`;
+  project_title.textContent = data.name;
+  github_button.setAttribute("href", data.github)
+  live_button.setAttribute("href", data.live)
+  project_description.innerHTML = data.description;
+  for (tag of data.tags) {
+    let span = document.createElement("span");
+    span.textContent = tag;
+    project_tags.appendChild(span);
+  }
+  return new_card;
+}
